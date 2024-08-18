87fffedecd92c3313b8442a9c8c026687c1a9be6
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-08-18 16:34:31 +0530

    added pages and dynamic route

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -17,7 +17,6 @@ const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="pages/profile" options={{ headerShown: false }} />
     </Stack>
   );
 };
