35a9cac5a05e8d7e2450af0dea8b3ff80a854406
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-08-18 20:01:27 +0530

    added axios configuration and environment variables

---
+++ api/axios.config.js
@@ -0,0 +1,25 @@
+/**
+ * Axios configuration
+ */
+
+import axios from "axios";
+import { API_BASE_URL } from "@env";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
